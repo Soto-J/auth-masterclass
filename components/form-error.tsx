cf928d537b835232227a6e0c9ed8805095d5999e
John Soto <soto.ja415@gmail.com>
2024-01-04 22:50:27 -0500

    Feat: Created form error component

---
+++ components/form-error.tsx
@@ -0,0 +1,31 @@
+import { ExclamationTriangleIcon } from "@radix-ui/react-icons";
+
+type FormErrorProps = {
+  message?: string;
+};
+
+const FormError = ({ message }: FormErrorProps) => {
+  if (!message) {
+    return null;
+  }
+
+  return (
+    <div
+      className="
+        bg-destructive/15
+        p-3
+        rounded-md
+        flex
+        items-center
+        gap-x-2
+        text-sm
+        text-destructive
+      "
+    >
+      <ExclamationTriangleIcon width={16} height={16} />
+      <p>{message}</p>
+    </div>
+  );
+};
+
+export default FormError;
