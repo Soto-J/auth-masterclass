f0ba85ef5e750cc414dd2ad7650de8be5f2ebf44
John Soto <soto.ja415@gmail.com>
2024-01-04 23:54:01 -0500

    Feat: Created register schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -2,5 +2,11 @@ import * as z from "zod";
 
 export const LoginSchema = z.object({
   email: z.string().min(6, { message: "Email required" }).email(),
-  password: z.string().min(6, { message: "Password required" }),
+  password: z.string().min(1, { message: "Password required" }),
+});
+
+export const RegisterSchema = z.object({
+  email: z.string().min(6, { message: "Email required" }).email(),
+  password: z.string().min(6, { message: "Minimum 6 characters required" }),
+  name: z.string().min(1, { message: "Name required" }),
 });
