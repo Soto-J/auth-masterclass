d837424ad57ff2dd0718eb0700256c6e19def73a
John Soto <soto.ja415@gmail.com>
2024-01-06 16:23:19 -0500

    Feat: Added middleware logic

---
+++ middleware.ts
@@ -0,0 +1,46 @@
+import NextAuth from "next-auth";
+import authConfig from "@/auth.config";
+
+export const { auth } = NextAuth(authConfig);
+
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  apiAuthPrefix,
+  authRoutes,
+  publicRoutes,
+} from "@/routes";
+
+export default auth((req) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+
+  console.log("ROUTE:", nextUrl.pathname);
+  console.log("IS LOGGED IN:", isLoggedIn);
+
+  // Needs to be checked first to avoid redirect loop
+  if (isApiAuthRoute) {
+    return null;
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, req.nextUrl));
+    }
+
+    return null;
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL("/auth/login", req.nextUrl));
+  }
+
+  return null;
+});
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
